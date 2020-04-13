d172b7cf03b10ac176023471c7461df6de6e6434
Spencer W. Jensen <spencer.jensen@Spencers-MacBook-Pro-5.local>
2020-04-13 14:49:24 -0600

    final push?

--- python/gofundmeapi/api/views.py
+++ python/gofundmeapi/api/views.py
@@ -41,60 +41,58 @@ class Predict(APIView):
             #serializer.save() # add this as a prediction to our database
 
             # here's where we do the connection with azure
-            azureData = {
-                "Inputs": {
-                    "input1": {
-                    "ColumnNames": [
-                        "Column 0",
-                        "name",
-                        "neighbourhood_group",
-                        "neighbourhood",
-                        "latitude",
-                        "longitude",
-                        "room_type",
-                        "price",
-                        "minimum_nights",
-                        "number_of_reviews",
-                        "calculated_host_listings_count",
-                        "availability_365"
+        azureData = {
+            "Inputs": {
+                "input1": {
+                "ColumnNames": [
+                    "Column 0",
+                    "name",
+                    "neighbourhood_group",
+                    "neighbourhood",
+                    "latitude",
+                    "longitude",
+                    "room_type",
+                    "price",
+                    "minimum_nights",
+                    "number_of_reviews",
+                    "calculated_host_listings_count",
+                    "availability_365"
+                ],
+                "Values": [
+                    [
+                    "value",
+                    request.data['title'],
+                    request.data['neighborhood'],
+                    "value",
+                    "0",
+                    "value",
+                    request.data['roomtype'],
+                    "0",
+                    request.data['minimumNights'],
+                    request.data['reviews'],
+                    "0",
+                    request.data['availability']
                     ],
-                    "Values": [
-                        [
-                        "value",
-                        request.data['title'],
-                        request.data['neighborhood'],
-                        "value",
-                        "0",
-                        "value",
-                        request.data['roomtype'],
-                        "0",
-                        request.data['minimumNights'],
-                        request.data['reviews'],
-                        "0",
-                        request.data['availability']
-                        ],
-                    ]
-                    }
-                },
-                "GlobalParameters": {}
-            }
-            
-            body = str.encode(json.dumps(azureData))
-            url = 'https://ussouthcentral.services.azureml.net/workspaces/ff6ae193fb884c889b6e523dc91a4a26/services/ce433886c30b4c14839b5d9b3baff778/execute?api-version=2.0&details=true'
-            api_key = '1kJu77SI+Ip81BzCGKYJTCm90JyNHB3k8wbln3ah8jBa9Uq19Jo+5HeAxQQV8GePFcyVKKGGk5YRJvT/Q8J46g=='
-            headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-            req = urllib.request.Request(url, body, headers) 
-            try:
-                response = urllib.request.urlopen(req)
-                result = response.read()
-                print("\n\n\n")
-                print(result) 
-                print("\n\n\n")
-                return Response(result)
-            except urllib.request.HTTPError as error:
-                print("The request failed with status code: " + str(error.code))
-                print(error.info())
-                print(json.loads(error.read()))
-                return Response(error, status=status.HTTP_400_BAD_REQUEST)
-        else:    
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                ]
+                }
+            },
+            "GlobalParameters": {}
+        }
+        
+        body = str.encode(json.dumps(azureData))
+        url = 'https://ussouthcentral.services.azureml.net/workspaces/ff6ae193fb884c889b6e523dc91a4a26/services/ce433886c30b4c14839b5d9b3baff778/execute?api-version=2.0&details=true'
+        api_key = '1kJu77SI+Ip81BzCGKYJTCm90JyNHB3k8wbln3ah8jBa9Uq19Jo+5HeAxQQV8GePFcyVKKGGk5YRJvT/Q8J46g=='
+        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+        req = urllib.request.Request(url, body, headers) 
+        try:
+            response = urllib.request.urlopen(req)
+            result = response.read()
+            print("\n\n\n")
+            print(result) 
+            print("\n\n\n")
+            return Response(result)
+        except urllib.request.HTTPError as error:
+            print("The request failed with status code: " + str(error.code))
+            print(error.info())
+            print(json.loads(error.read()))
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
--- python/gofundmeapi/gofundmeapi/settings.py
+++ python/gofundmeapi/gofundmeapi/settings.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from base import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-SECRET_KEY = 'hrm8fo#nk-#mgr+-=gqm_9*bnwqzm6&(pc+ug#_gr4rzxm1(8l'
+SECRET_KEY = "hrm8fo#nk-#mgr+-=gqm_9*bnwqzm6&(pc+ug#_gr4rzxm1(8l"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
